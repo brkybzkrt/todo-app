969a797eaf0f6b8958b77dade81d363c1a1b221d
brkybzkrt <b9bozkurt959@gmail.com>
2025-02-13 23:50:49 +0300

    update password was added to auth controller and route

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { register, login } = require("../controllers/auth.controller");
+const { register, login, updatePassword } = require("../controllers/auth.controller");
+const authMiddleware = require("../middlewares/auth");
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.patch("/update-password", authMiddleware, updatePassword);
 
 module.exports = router;
