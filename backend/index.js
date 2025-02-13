665d07b244a8564fbbc9c61736a423decfc42262
brkybzkrt <b9bozkurt959@gmail.com>
2025-02-13 19:46:57 +0300

    some dependencies were added and index.js was created.

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`🚀 Server is running on http://localhost:${PORT}`));
