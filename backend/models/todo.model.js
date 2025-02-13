81a5c04bb1d8fd32fee2abb7ff759550d9f5d3af
brkybzkrt <b9bozkurt959@gmail.com>
2025-02-13 22:39:56 +0300

    The isDeleted field was added to Todo model

--- backend/models/todo.model.js
+++ backend/models/todo.model.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const TodoSchema = new mongoose.Schema({
     title: { type: String, required: true },
     completed: { type: Boolean, default: false },
+    isDeleted: { type: Boolean, default: false },
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
